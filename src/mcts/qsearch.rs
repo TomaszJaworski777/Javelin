347b1984c3b1c93f98aeafb953799edef6a7e242
Snekkers <71073337+TomaszJaworski777@users.noreply.github.com>
2024-05-04 19:41:41 +0200

    Max depth safecheck

--- src/mcts/qsearch.rs
+++ src/mcts/qsearch.rs
@@ -13,6 +13,10 @@ pub fn qsearch<'a>(board: &Board, mut alpha: i32, beta: i32, depth: u8) -> i32 {
 
     let evaluation = Evaluation::evaluate(&board);
 
+    if depth > 128{
+        return evaluation;
+    }
+
     if evaluation >= beta {
         return beta;
     }
