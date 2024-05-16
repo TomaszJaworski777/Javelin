bbdd62f6c6f1e02f05f46f01916c2eda61ee7b5b
Snekkers <71073337+TomaszJaworski777@users.noreply.github.com>
2024-05-17 00:16:56 +0200

    Moved activation function for better memory usage

--- src/eval/value_network.rs
+++ src/eval/value_network.rs
@@ -13,8 +13,8 @@ const SIGMOID_FUNCTION: u8 = 3;
 #[repr(C)]
 #[derive(Clone, Copy)]
 pub struct ValueNetwork {
-    input_layer: NetworkLayer<768, 16, SCRELU_FUNCTION>,
-    output_layer: NetworkLayer<16, 1, NO_FUNCTION>,
+    input_layer: NetworkLayer<768, 16, NO_FUNCTION>,
+    output_layer: NetworkLayer<16, 1, SCRELU_FUNCTION>,
 }
 #[allow(unused)]
 impl ValueNetwork {
